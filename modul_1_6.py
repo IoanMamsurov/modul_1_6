5290467df18119c872b8eeeee6843ceb33cac645
IoanMamsurov <ivan.mamsurov@yandex.ru>
2024-08-16 00:15:11 +0300

    Add files via upload

---
+++ modul_1_6.py
@@ -0,0 +1,14 @@
+my_dict = {'apple': 1, 'orange': 2, 'milk': 3, 'cucumber': 4}
+print(my_dict)
+print(my_dict.get('orange'))
+print(my_dict.get('fase'))
+my_dict.update({'car': 8, 'dog': 9})
+a = my_dict.pop('milk')
+print(a)
+print(my_dict)
+my_set = {1,2,3,4,5,6,1,2,3,4,8,9,3,}
+print(my_set)
+my_set.add(13)
+my_set.add(12)
+my_set.discard(5)
+print(my_set)
